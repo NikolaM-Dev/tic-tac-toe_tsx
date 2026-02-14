1371e64213e4e5488d0527ede529e9d2bf9b0dfe
Juan David Merchan Torres <juan1014166@gmail.com>
2026-02-14 17:06:15 -0500

    refactor(src): Change to use components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Board } from './components/Board';
+import { History } from './components/History';
+
 export function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-8 bg-neutral-900 p-32 text-neutral-200">
+      <h1 className="text-8xl font-bold">Tic Tac Toe</h1>
+      <h2 className="text-4xl">Game Status</h2>
+      <Board />
+      <History />
+    </div>
   );
 }
