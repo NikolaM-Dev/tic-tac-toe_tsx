b9d360f69d4414cd323b3df651d4aff47b65ba60
Juan David Merchan Torres <juan1014166@gmail.com>
2026-02-18 13:53:24 -0500

    fix(Button): Remove unnecessary fragment

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,16 +10,14 @@ export function Button({
   ...props
 }: ButtonProps): React.JSX.Element {
   return (
-    <>
-      <button
-        className={twMerge(
-          'rounded-md border border-transparent bg-slate-700 px-4 py-2 text-center text-sm text-white shadow-md transition-all hover:bg-slate-500 hover:shadow-lg focus:shadow-none active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none',
-          className,
-        )}
-        {...props}
-      >
-        {children}
-      </button>
-    </>
+    <button
+      className={twMerge(
+        'rounded-md border border-transparent bg-slate-700 px-4 py-2 text-center text-sm text-white shadow-md transition-all hover:bg-slate-500 hover:shadow-lg focus:shadow-none active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
   );
 }
