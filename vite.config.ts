f18c25e5c5001b7b71343053d52466d3a480ff22
Juan David Merchan Torres <juan1014166@gmail.com>
2026-02-18 13:31:57 -0500

    ci(root): Fix the base url to deploy in GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/NikolaM-Dev/tic-tac-toe_tsx',
+  base: '/tic-tac-toe_tsx/',
   plugins: [react(), tailwindcss()],
 });
